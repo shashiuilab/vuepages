b2620be4bf6ea3e045df151b2ccf8f97a001951c
shashiuilab <40182776+shashiuilab@users.noreply.github.com>
2018-06-12 22:30:33 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="assets/logo.png">
+    <img src="../../assets/logo.png">
     <p>{{msg}}</p>
     <app-header :message="msg"></app-header>
     <h2>Essential Links</h2>
